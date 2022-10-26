81168ad48c0a434318decdff0159937c2328aac0
Kendrick Winata <kendrickwinata@outlook.com>
2022-10-27 01:33:49 +0800

    feat: models views and serializer updated for order

--- base/models.py
+++ base/models.py
@@ -40,8 +40,7 @@ class Review(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
     taxPrice = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
@@ -50,11 +49,12 @@ class Order(models.Model):
     totalPrice = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
     isPaid = models.BooleanField(default=False)
-    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    paidAt = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(default=False)
     deliveredAt = models.DateTimeField(
         auto_now_add=False, null=True, blank=True)
-    createdAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
