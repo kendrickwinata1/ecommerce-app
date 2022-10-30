88db2e2b4faddac975dd7ed1b0231e5fe929189d
Kendrick Winata <kendrickwinata@outlook.com>
2022-10-30 21:02:27 +0800

    update

--- backend/settings.py
+++ backend/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-rw@hzg*r1dps)f@u(3sl%f2id-1oe+^qp9qb2&k1@$jv7*b3ez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 'caspershop.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'caspershop.herokuapp.com']
 
 
 # Application definition
